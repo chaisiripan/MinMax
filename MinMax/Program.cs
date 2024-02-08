234ad26bf395e05ad5b2d0debf20942232efafab
Pongpat Chaisiripan <pongpat.cha@siam.edu>
2024-02-08 13:09:45 +0700

    Add Byte Int

--- MinMax/Program.cs
+++ MinMax/Program.cs
@@ -12,6 +12,16 @@ namespace MinMax
         {
             Console.WriteLine("Min sbyte = " + SByte.MinValue);
             Console.WriteLine("Max sbyte = " + SByte.MaxValue);
+            Console.WriteLine("Min byte = " + Byte.MinValue);
+            Console.WriteLine("Max byte = " + Byte.MaxValue);
+            Console.WriteLine("Min short = " + Int16.MinValue);
+            Console.WriteLine("Max short = " + Int16.MaxValue);
+            Console.WriteLine("Min ushort = " + UInt16.MinValue);
+            Console.WriteLine("Max ushort = " + UInt16.MaxValue);
+            Console.WriteLine("Min int = " + Int32.MinValue);
+            Console.WriteLine("Max int = " + Int32.MaxValue);
+            Console.WriteLine("Min uint = " + UInt32.MinValue);
+            Console.WriteLine("Max uint = " + UInt32.MaxValue);
         }
     }
 }
